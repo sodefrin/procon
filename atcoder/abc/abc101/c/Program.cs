c73c68576e4fbd5c153e589e7dd034037c50d0f4
sodefrin <kojitazoe@gmail.com>
2019-06-19 03:01:29 +0900

    Solved abc101 a,b,c

---
+++ atcoder/abc/abc101/c/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Procon
+{
+    public Procon() { }
+    static void Main(string[] agrs)
+    {
+        new Procon().Do();
+    }
+
+    Scanner cin;
+
+    public void Do()
+    {
+        cin = new Scanner();
+        long N = cin.nextLong();
+        long K = cin.nextLong();
+
+        long pos = 0;
+        for (int i = 0; i < N; i++)
+        {
+            long a = cin.nextLong();
+            if (a == 1)
+            {
+                pos = i;
+            }
+        }
+
+        long ret = 1000000000;
+        for (int i = 0; i < K; i++)
+        {
+            long left_num = pos - i;
+            long right_num = N - pos - 1 - (K - 1 - i);
+            if (left_num == 0 && right_num == 0)
+            {
+                ret = Math.Min(1, ret);
+                continue;
+            }
+            if (left_num < 0 || right_num < 0)
+            {
+                continue;
+            }
+            ret = Math.Min(1 + (left_num + K - 2) / (K - 1) + (right_num + K - 2) / (K - 1), ret);
+        }
+        Console.WriteLine(ret);
+    }
+}
+
+class Scanner
+{
+    string[] s;
+    int i;
+    int max_i;
+
+    public Scanner()
+    {
+        s = new string[0];
+        max_i = 0;
+        i = 0;
+    }
+
+    public string nextString()
+    {
+        if (i >= s.Length)
+        {
+            s = Console.ReadLine().Split(' ');
+            max_i = s.Length;
+            i = 0;
+            if (max_i == 0)
+            {
+                return "";
+            }
+            return s[i++];
+        }
+        else
+        {
+            return s[i++];
+        }
+    }
+
+    public int nextInt()
+    {
+        return int.Parse(nextString());
+    }
+
+
+    public long nextLong()
+    {
+        return long.Parse(nextString());
+    }
+
+    public double nextDouble()
+    {
+        return double.Parse(nextString());
+    }
+
+    public string[] arrayString()
+    {
+        return Console.ReadLine().Split(' ');
+    }
+
+    public int[] arrayInt()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        int[] array = new int[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = int.Parse(str[i]);
+        }
+        return array;
+    }
+
+    public long[] arrayLong()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        long[] array = new long[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = long.Parse(str[i]);
+        }
+        return array;
+    }
+
+    public double[] arrayDouble()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        double[] array = new double[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = double.Parse(str[i]);
+        }
+        return array;
+    }
+}
