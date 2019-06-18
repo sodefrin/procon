f1d3f3353f2fc21f26b1270f871b10fa85096c30
sodefrin <kojitazoe@gmail.com>
2019-06-18 18:34:16 +0900

    Solved abc104 a,b,c

---
+++ atcoder/abc/abc104/b/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Procon
+{
+    public Procon() { }
+    static void Main(string[] agrs)
+    {
+        new Procon().Do();
+    }
+
+    Scanner cin;
+
+    public void Do()
+    {
+        cin = new Scanner();
+        string S = cin.nextString();
+        int a = -1;
+        int b = -1;
+        for (int i = 0; i < S.Length; i++)
+        {
+            if (S[i] == 'A' && a == -1)
+            {
+                a = i;
+            }
+            else if (S[i] == 'C' && b == -1)
+            {
+                b = i;
+            }
+            else
+            {
+                if (!Char.IsLower(S[i]))
+                {
+                    Console.WriteLine("WA");
+                    return;
+                }
+            }
+        }
+
+        if (a != 0)
+        {
+            Console.WriteLine("WA");
+            return;
+        }
+
+        if (!(2 <= b && b <= S.Length - 2))
+        {
+            Console.WriteLine("WA");
+            return;
+        }
+
+        Console.WriteLine("AC");
+    }
+}
+
+class Scanner
+{
+    string[] s;
+    int i;
+    int max_i;
+
+    public Scanner()
+    {
+        s = new string[0];
+        max_i = 0;
+        i = 0;
+    }
+
+    public string nextString()
+    {
+        if (i >= s.Length)
+        {
+            s = Console.ReadLine().Split(' ');
+            max_i = s.Length;
+            i = 0;
+            if (max_i == 0)
+            {
+                return "";
+            }
+            return s[i++];
+        }
+        else
+        {
+            return s[i++];
+        }
+    }
+
+    public int nextInt()
+    {
+        return int.Parse(nextString());
+    }
+
+
+    public long nextLong()
+    {
+        return long.Parse(nextString());
+    }
+
+    public double nextDouble()
+    {
+        return double.Parse(nextString());
+    }
+
+    public string[] arrayString()
+    {
+        return Console.ReadLine().Split(' ');
+    }
+
+    public int[] arrayInt()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        int[] array = new int[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = int.Parse(str[i]);
+        }
+        return array;
+    }
+
+    public long[] arrayLong()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        long[] array = new long[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = long.Parse(str[i]);
+        }
+        return array;
+    }
+
+    public double[] arrayDouble()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        double[] array = new double[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = double.Parse(str[i]);
+        }
+        return array;
+    }
+}
---
+++ atcoder/abc/abc104/c/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Procon
+{
+    public Procon() { }
+    static void Main(string[] agrs)
+    {
+        new Procon().Do();
+    }
+
+    Scanner cin;
+
+    int D;
+    long G;
+    long[] p;
+    long[] c;
+
+    public void Do()
+    {
+        cin = new Scanner();
+        D = cin.nextInt();
+        G = cin.nextLong();
+        p = new long[D];
+        c = new long[D];
+        for (int i = 0; i < D; i++)
+        {
+            p[i] = cin.nextLong();
+            c[i] = cin.nextLong();
+        }
+        Console.WriteLine(dfs(0, 0));
+    }
+
+    long solveToG(int isComplete)
+    {
+        long score = 0;
+        long num = 0;
+        for (int i = 1; i <= D; i++)
+        {
+            if ((isComplete >> i & 1) == 1)
+            {
+                score += c[i - 1] + i * 100 * p[i - 1];
+                num += p[i - 1];
+            }
+        }
+
+        if (score >= G)
+        {
+            return num;
+        }
+
+        long remaining = G - score;
+        for (int i = D; i >= 1; i--)
+        {
+            if ((isComplete >> i & 1) == 1)
+            {
+                continue;
+            }
+            if (remaining <= i * 100 * p[i - 1])
+            {
+                num += remaining / (i * 100);
+                if (remaining % (i * 100) != 0)
+                {
+                    num++;
+                }
+                return num;
+            }
+            else
+            {
+                num += p[i - 1];
+                remaining -= i * 100 * p[i - 1];
+            }
+        }
+        return 100000000;
+    }
+
+    long dfs(int n, int isComplete)
+    {
+        if (n == D)
+        {
+            return solveToG(isComplete);
+        }
+
+        return Math.Min(dfs(n + 1, isComplete), dfs(n + 1, isComplete + (1 << n + 1)));
+    }
+}
+
+class Scanner
+{
+    string[] s;
+    int i;
+    int max_i;
+
+    public Scanner()
+    {
+        s = new string[0];
+        max_i = 0;
+        i = 0;
+    }
+
+    public string nextString()
+    {
+        if (i >= s.Length)
+        {
+            s = Console.ReadLine().Split(' ');
+            max_i = s.Length;
+            i = 0;
+            if (max_i == 0)
+            {
+                return "";
+            }
+            return s[i++];
+        }
+        else
+        {
+            return s[i++];
+        }
+    }
+
+    public int nextInt()
+    {
+        return int.Parse(nextString());
+    }
+
+
+    public long nextLong()
+    {
+        return long.Parse(nextString());
+    }
+
+    public double nextDouble()
+    {
+        return double.Parse(nextString());
+    }
+
+    public string[] arrayString()
+    {
+        return Console.ReadLine().Split(' ');
+    }
+
+    public int[] arrayInt()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        int[] array = new int[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = int.Parse(str[i]);
+        }
+        return array;
+    }
+
+    public long[] arrayLong()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        long[] array = new long[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = long.Parse(str[i]);
+        }
+        return array;
+    }
+
+    public double[] arrayDouble()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        double[] array = new double[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = double.Parse(str[i]);
+        }
+        return array;
+    }
+}
