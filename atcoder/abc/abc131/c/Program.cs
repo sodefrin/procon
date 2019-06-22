d28e649102f76461367fc44b5ca2a5f36a8eac68
sodefrin <kojitazoe@gmail.com>
2019-06-22 22:46:41 +0900

    Solved abc131 a,b,c,d,e

---
+++ atcoder/abc/abc131/c/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Procon
+{
+    public Procon() { }
+    static void Main(string[] agrs)
+    {
+        new Procon().Do();
+    }
+
+    Scanner cin;
+
+    public void Do()
+    {
+        cin = new Scanner();
+        long A = cin.nextLong();
+        long B = cin.nextLong();
+        long C = cin.nextLong();
+        long D = cin.nextLong();
+
+        long ans = B - A + 1 - (calc(B, C, D) - calc(A - 1, C, D));
+        Console.WriteLine(ans);
+    }
+
+    long calc(long N, long C, long D)
+    {
+        return N / C + N / D - N / (C * D / gcd(C, D));
+    }
+
+    long gcd(long a, long b)
+    {
+        long tmp;
+        if (b > a)
+        {
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        if (b == 0)
+        {
+            return a;
+        }
+
+        return gcd(b, a % b);
+    }
+}
+
+class Scanner
+{
+    string[] s;
+    int i;
+    int max_i;
+
+    public Scanner()
+    {
+        s = new string[0];
+        max_i = 0;
+        i = 0;
+    }
+
+    public string nextString()
+    {
+        if (i >= s.Length)
+        {
+            s = Console.ReadLine().Split(' ');
+            max_i = s.Length;
+            i = 0;
+            if (max_i == 0)
+            {
+                return "";
+            }
+            return s[i++];
+        }
+        else
+        {
+            return s[i++];
+        }
+    }
+
+    public int nextInt()
+    {
+        return int.Parse(nextString());
+    }
+
+
+    public long nextLong()
+    {
+        return long.Parse(nextString());
+    }
+
+    public double nextDouble()
+    {
+        return double.Parse(nextString());
+    }
+
+    public string[] arrayString()
+    {
+        return Console.ReadLine().Split(' ');
+    }
+
+    public int[] arrayInt()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        int[] array = new int[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = int.Parse(str[i]);
+        }
+        return array;
+    }
+
+    public long[] arrayLong()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        long[] array = new long[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = long.Parse(str[i]);
+        }
+        return array;
+    }
+
+    public double[] arrayDouble()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        double[] array = new double[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = double.Parse(str[i]);
+        }
+        return array;
+    }
+}
+
---
+++ atcoder/abc/abc131/e/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Procon
+{
+    public Procon() { }
+    static void Main(string[] agrs)
+    {
+        new Procon().Do();
+    }
+
+    Scanner cin;
+
+    public void Do()
+    {
+        cin = new Scanner();
+        int N = cin.nextInt();
+        int K = cin.nextInt();
+
+        int max = (N - 1) * (N - 2) / 2;
+        if (K > max)
+        {
+            Console.WriteLine(-1);
+            return;
+        }
+
+        Console.WriteLine(N - 1 + max - K);
+        for (int i = 2; i <= N; i++)
+        {
+            Console.WriteLine("{0} {1}", 1, i);
+        }
+
+        int cur = 2;
+        int tmp = 1;
+        for (int i = 0; i < max - K; i++)
+        {
+            if (cur + tmp > N)
+            {
+                cur++;
+                tmp = 1;
+            }
+            Console.WriteLine("{0} {1}", cur, cur + tmp);
+            tmp++;
+        }
+    }
+}
+
+class Scanner
+{
+    string[] s;
+    int i;
+    int max_i;
+
+    public Scanner()
+    {
+        s = new string[0];
+        max_i = 0;
+        i = 0;
+    }
+
+    public string nextString()
+    {
+        if (i >= s.Length)
+        {
+            s = Console.ReadLine().Split(' ');
+            max_i = s.Length;
+            i = 0;
+            if (max_i == 0)
+            {
+                return "";
+            }
+            return s[i++];
+        }
+        else
+        {
+            return s[i++];
+        }
+    }
+
+    public int nextInt()
+    {
+        return int.Parse(nextString());
+    }
+
+
+    public long nextLong()
+    {
+        return long.Parse(nextString());
+    }
+
+    public double nextDouble()
+    {
+        return double.Parse(nextString());
+    }
+
+    public string[] arrayString()
+    {
+        return Console.ReadLine().Split(' ');
+    }
+
+    public int[] arrayInt()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        int[] array = new int[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = int.Parse(str[i]);
+        }
+        return array;
+    }
+
+    public long[] arrayLong()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        long[] array = new long[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = long.Parse(str[i]);
+        }
+        return array;
+    }
+
+    public double[] arrayDouble()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+        double[] array = new double[str.Length];
+        for (int i = 0; i < str.Length; i++)
+        {
+            array[i] = double.Parse(str[i]);
+        }
+        return array;
+    }
+}
+
