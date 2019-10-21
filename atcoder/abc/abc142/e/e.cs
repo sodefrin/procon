61993b1d376689ca76b636f42ab6236577c94857
sodefrin <kojitazoe@gmail.com>
2019-10-21 21:24:21 +0900

    Solved abc 142

---
+++ atcoder/abc/abc142/e/e.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+class Procon {
+    static void Main (string[] agrs) {
+        int N = Int();
+        int M = Int();
+
+        int size = 1<<N;
+        int[] DP = new int[size];
+
+        for (int i = 0; i<size; i++) {
+            DP[i] = Mod;
+        }
+
+        for (int i = 0; i<M; i++) {
+            int a = Int();
+            int b = Int();
+            int key = 0;
+            for (int j=0; j<b; j++) {
+                int c = Int();
+                key += 1 << (c -1);
+            }
+
+            DP[key] = Math.Min(DP[key], a);
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                DP[i | j] = Math.Min(DP[i | j], DP[i] + DP[j]);
+            }
+        }
+
+        if (DP[size-1] == Mod) {
+            Console.WriteLine(-1);
+            return;
+        }
+
+        Console.WriteLine(DP[size-1]);
+    }
+
+    static string String () => Scanner.nextString ();
+    static int Int () => int.Parse (Scanner.nextString ());
+    static long Long () => long.Parse (Scanner.nextString ());
+    static double Double () => double.Parse (Scanner.nextString ());
+    static char[] Chars () => String ().ToCharArray ();
+    static string[] Strings () => Console.ReadLine ().Split (' ');
+    static int[] Ints () => Strings ().Select (v => int.Parse (v)).ToArray ();
+    static long[] Longs () => Strings ().Select (v => long.Parse (v)).ToArray ();
+    static double[] Doubles () => Strings ().Select (v => double.Parse (v)).ToArray ();
+    const int Mod = 1000000007;
+
+    static long gcd(long a, long b)
+    {
+        long tmp;
+        if (b > a)
+        {
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        if (b == 0)
+        {
+            return a;
+        }
+
+        return gcd(b, a % b);
+    }
+}
+
+class PriorityQueue<T> where T : IComparable<T> {
+    public T[] heap;
+    public int size;
+    public int sign;
+    public PriorityQueue (int N, bool descend = false) {
+        heap = new T[N];
+        sign = 1;
+        if (descend) sign = -1;
+    }
+    public int Compare (T x, T y) {
+        return x.CompareTo (y) * sign;
+    }
+    public void Push (T x) {
+        int i = size++;
+        while (i > 0) {
+            int p = (i - 1) / 2;
+            if (Compare (x, heap[p]) >= 0) {
+                break;
+            }
+            heap[i] = heap[p];
+            i = p;
+        }
+        heap[i] = x;
+    }
+    public T Pop () {
+        T ret = heap[0];
+        T x = heap[--size];
+        int i = 0;
+        while (i * 2 + 1 < size) {
+            int a = i * 2 + 1;
+            int b = i * 2 + 2;
+            if (b < size && Compare (heap[a], heap[b]) > 0) {
+                a = b;
+            }
+            if (Compare (heap[a], x) >= 0) {
+                break;
+            }
+            heap[i] = heap[a];
+            i = a;
+        }
+        heap[i] = x;
+        return ret;
+    }
+    public int Count () {
+        return size;
+    }
+}
+
+class Scanner {
+    static string[] s = new string[0];
+    static int i = 0;
+    static int max_i = 0;
+    static public string nextString () {
+        if (i >= s.Length) {
+            s = Console.ReadLine ().Split (' ');
+            max_i = s.Length;
+            i = 0;
+            if (max_i == 0) {
+                return "";
+            }
+            return s[i++];
+        }
+        return s[i++];
+    }
+}
